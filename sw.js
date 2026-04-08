69ccf0733955117f1b09935e50b97aee125f1e9d
Leah Braunstein <leah@befinch.com>
2026-04-08 10:27:25 -0700

    Initial commit

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'kiln-diary-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;1,400;1,500&family=Lato:wght@300;400&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // For navigation requests, serve index.html from cache
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      caches.match('/index.html').then(r => r || fetch(e.request))
+    );
+    return;
+  }
+  // For everything else: cache-first, fall back to network
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res && res.status === 200 && e.request.method === 'GET') {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+      }
+      return res;
+    }))
+  );
+});
